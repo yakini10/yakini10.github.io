ad07255fc4e8053124eb0502600638648ffb2352
yakini10 <temeaissataditeyakine@gmail.com>
2026-03-02 23:18:01 +0300

    Update form.php

--- exo3/form.php
+++ exo3/form.php
@@ -1,12 +1,63 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Анкета</title>
+</head>
+<body>
+
 <form action="" method="POST">
-  <input name="fio" />
-  <select name="year">
-    <?php 
-    for ($i = 1922; $i <= 2022; $i++) {
-      printf('<option value="%d">%d год</option>', $i, $i);
-    }
-    ?>
-  </select>
   
-  <input type="submit" value="ok" />
+  <!-- 1) ФИО -->
+  <label>ФИО:</label><br>
+  <input type="text" name="fio" required><br><br>
+
+  <!-- 2) Телефон -->
+  <label>Телефон:</label><br>
+  <input type="tel" name="phone" required><br><br>
+
+  <!-- 3) Email -->
+  <label>E-mail:</label><br>
+  <input type="email" name="email" required><br><br>
+
+  <!-- 4) Дата рождения -->
+  <label>Дата рождения:</label><br>
+  <input type="date" name="birthdate" required><br><br>
+
+  <!-- 5) Пол -->
+  <label>Пол:</label><br>
+  <input type="radio" name="gender" value="male" required> Мужской<br>
+  <input type="radio" name="gender" value="female"> Женский<br><br>
+
+  <!-- 6) Любимый язык программирования -->
+  <label>Любимый язык программирования:</label><br>
+  <select name="languages[]" multiple size="6" required>
+    <option value="Pascal">Pascal</option>
+    <option value="C">C</option>
+    <option value="C++">C++</option>
+    <option value="JavaScript">JavaScript</option>
+    <option value="PHP">PHP</option>
+    <option value="Python">Python</option>
+    <option value="Java">Java</option>
+    <option value="Haskel">Haskel</option>
+    <option value="Clojure">Clojure</option>
+    <option value="Prolog">Prolog</option>
+    <option value="Scala">Scala</option>
+    <option value="Go">Go</option>
+  </select><br><br>
+
+  <!-- 7) Биография -->
+  <label>Биография:</label><br>
+  <textarea name="bio" rows="5" cols="40" required></textarea><br><br>
+
+  <!-- 8) С контрактом ознакомлен(а) -->
+  <input type="checkbox" name="contract" value="yes" required>
+  С контрактом ознакомлен(а)<br><br>
+
+  <!-- 9) Кнопка сохранить -->
+  <input type="submit" value="Сохранить">
+
 </form>
+
+</body>
+</html>
