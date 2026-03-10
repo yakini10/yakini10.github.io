d98b577c7de856809c7cf11f1c675f73fcb7d782
yakini10 <temeaissataditeyakine@gmail.com>
2026-03-10 12:16:57 +0300

    Update form.php

--- exo3/form.php
+++ exo3/form.php
@@ -32,18 +32,18 @@
   <!-- 6) Любимый язык программирования -->
   <label>Любимый язык программирования:</label><br>
   <select name="languages[]" multiple size="6" required>
-    <option value="Pascal">Pascal</option>
-    <option value="C">C</option>
-    <option value="C++">C++</option>
-    <option value="JavaScript">JavaScript</option>
-    <option value="PHP">PHP</option>
-    <option value="Python">Python</option>
-    <option value="Java">Java</option>
-    <option value="Haskel">Haskel</option>
-    <option value="Clojure">Clojure</option>
-    <option value="Prolog">Prolog</option>
-    <option value="Scala">Scala</option>
-    <option value="Go">Go</option>
+    <option value="1">Pascal">Pascal</option>
+    <option value="2">C</option>
+    <option value="3">C++</option>
+    <option value="4">JavaScript</option>
+    <option value="5">PHP</option>
+    <option value="6">Python</option>
+    <option value="7">Java</option>
+    <option value="8">Haskel</option>
+    <option value="9">Clojure</option>
+    <option value="10">Prolog</option>
+    <option value="11">Scala</option>
+    <option value="12">Go</option>
   </select><br><br>
 
   <!-- 7) Биография -->
