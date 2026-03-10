f02490ea15f523dda13ecbe25b9726806131233d
yakini10 <temeaissataditeyakine@gmail.com>
2026-03-10 11:56:28 +0300

    Update index.php

--- exo3/index.php
+++ exo3/index.php
@@ -20,76 +20,85 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
 // Проверяем ошибки.
 $errors = [];
+
 if (empty($_POST['fio'])) {
-  $errors [] ='Заполните имя.';
+  $errors[] = 'Заполните имя.';
 }
 
 if (empty($_POST['phone'])) {
-  $errors[] = 'Заполните телефон';
+  $errors[] = 'Заполните телефон.';
 }
+
 if (empty($_POST['email'])) {
-  $errors[] = 'Заполните email';
-} elseif (!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
-  $errors[]='Некорректный email';
+  $errors[] = 'Заполните email.';
+} elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  $errors[] = 'Некорректный email.';
 }
+
 if (empty($_POST['birth_date'])) {
-  $errors[] = 'Заполните дату рождения';
+  $errors[] = 'Заполните дату рождения.';
 }
+
 if (empty($_POST['gender'])) {
-  $errors[] = 'Выберите пол';
+  $errors[] = 'Выберите пол.';
 }
+
 if (empty($_POST['languages']) || !is_array($_POST['languages'])) {
-  $errors[] = 'Выберите хотя бы один язык программирования';
+  $errors[] = 'Выберите хотя бы один язык программирования.';
 }
+
 if (empty($_POST['biography'])) {
-  $errors[] = 'Заполните биографию';
+  $errors[] = 'Заполните биографию.';
 }
+
 if (empty($_POST['contract_accepted'])) {
-  $errors []= 'Необходимо подверждение для заключение контракта';
+  $errors[] = 'Необходимо подтверждение для заключения контракта.';
 }
+
+// S'il y a des erreurs, on les affiche ET on inclut le formulaire
 if (!empty($errors)) {
-  foreach ($errors as $error){
+  foreach ($errors as $error) {
     print($error . "<br>");
   }
   include('form.php');
   exit();
-}
+} else {
+  // *************
+  // Тут необходимо проверить правильность заполнения всех остальных полей.
+  // *************
 
-// *************
-// Тут необходимо проверить правильность заполнения всех остальных полей.
-// *************
+  // Сохранение в базу данных.
+  $user = 'u82383'; // Заменить на ваш логин uXXXXX
+  $pass = 'dt54#FDrt'; // Заменить на пароль
+  $db = new PDO('mysql:host=localhost;dbname=u82383', $user, $pass,
+    [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
 
-
-// Сохранение в базу данных.
-
-$user = 'u82383'; // Заменить на ваш логин uXXXXX
-$pass = 'dt54#FDrt'; // Заменить на пароль
-$db = new PDO('mysql:host=localhost;dbname=u82383', $user, $pass,
-  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
-
-// Подготовленный запрос. Не именованные метки.
-try {
-  $stmt = $db->prepare("INSERT INTO application (fio, phone, email, birthdate, gender, bio, contract) VALUES (?,?,?,?,?,?,?)");
-  $stmt ->execute([
-    $_POST['fio'], 
-    $_POST['phone'],
-    $_POST['email'],
-    $_POST['birth_date'],
-    $_POST['gender'],
-    $_POST['biography'],
-    isset($_POST['contract_accepted'])? 1 : 0
+  // Подготовленный запрос. Не именованные метки.
+  try {
+    $stmt = $db->prepare("INSERT INTO application (fio, phone, email, birthdate, gender, bio, contract) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $stmt->execute([
+      $_POST['fio'], 
+      $_POST['phone'],
+      $_POST['email'],
+      $_POST['birth_date'],
+      $_POST['gender'],
+      $_POST['biography'],
+      isset($_POST['contract_accepted']) ? 1 : 0
     ]);
-    $application_id = $db -> lastInsertId();
-    $stmt = $db->prepare("INSERT INTO application_languages(application_id,language_id)VALUES(?,?)");
-    foreach($_POST['languages'] as $lang_id) {
-      $stmt->execute([$application_id,$lang_id]);
+    
+    $application_id = $db->lastInsertId();
+    $stmt = $db->prepare("INSERT INTO application_languages (application_id, language_id) VALUES (?, ?)");
+    
+    foreach ($_POST['languages'] as $lang_id) {
+      $stmt->execute([$application_id, $lang_id]);
     }
-    header('location: ?save=1');
+    
+    header('Location: ?save=1');
     exit();
-
-}
-catch(PDOException $e){
-  print('Error : ' . $e->getMessage());
-  exit();
+    
+  } catch (PDOException $e) {
+    print('Error : ' . $e->getMessage());
+    exit();
+  }
 }
 ?>
