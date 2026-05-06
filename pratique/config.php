313d69255ae161f5018294017f70183b89ac75cb
yakini10 <temeaissataditeyakine@gmail.com>
2026-05-06 21:08:33 +0300

    Update config.php

--- pratique/config.php
+++ pratique/config.php
@@ -4,9 +4,12 @@ define('DB_NAME', 'u82383');
 define('DB_USER', 'u82383');
 define('DB_PASS', 'dt54#FDrt');
 
-
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO(
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8",
+        DB_USER,
+        DB_PASS
+    );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Erreur : " . $e->getMessage());
