9cf1c5447dd7a2aaed8fcf78843e1120be41c283
yakini10 <temeaissataditeyakine@gmail.com>
2026-05-06 20:32:57 +0300

    Create config.php

---
+++ pratique/config.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost';
+$dbname = 'clinique_veto';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Erreur : " . $e->getMessage());
+}
+
+session_start();
+?>
