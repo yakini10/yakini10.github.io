9dbd2669ad310d52ca9ac3d9cf956d9b6b8841c8
yakini10 <temeaissataditeyakine@gmail.com>
2026-05-07 21:44:59 +0300

    Update index.php

--- pratique/index.php
+++ pratique/index.php
@@ -159,7 +159,7 @@ $proprietaires = $pdo->query("SELECT * FROM proprietaires")->fetchAll();
             <a href="#visites"> Визиты</a>
             <a href="#filtrer"> Фильтр</a>
             <a href="form.php"> Добавить животное</a>
-            <a href="visite_ajouter.php" style="background: #17a2b8;"> Добавить визит</a>
+            <a href="visite_ajouter.php"> Добавить визит</a>
         </div>
     </div>
 
