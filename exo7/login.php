7dc4f83a5d4ea087b800694b060db30cd72fb6a6
yakini10 <temeaissataditeyakine@gmail.com>
2026-05-10 01:35:48 +0300

    Update login.php

--- exo7/login.php
+++ exo7/login.php
@@ -1,1 +1,28 @@
+<?php
+session_start();
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Login</title>
+</head>
+<body>
+
+<h1>Login</h1>
+
+<form action="index.php" method="POST">
+
+<input type="hidden" name="csrf" value="<?= $_SESSION['csrf'] ?>">
+<input type="hidden" name="auth" value="1">
+
+<label>Login</label>
+<input type="text" name="login">
+
+<br><br>
+
+<label>Password</label>
+<input type="password" name="password">
+
+<br><br>
