c682249a613f7d15c5161aa211ce7cdad7680b51
yakini10 <temeaissataditeyakine@gmail.com>
2026-05-09 19:09:19 +0300

    Create config.php

---
+++ essaie/config.php
@@ -0,0 +1,19 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'u82383');
+define('DB_USER', 'u82383');
+define('DB_PASS', 'dt54#FDrt');
+
+try {
+    $pdo = new PDO(
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8",
+        DB_USER,
+        DB_PASS
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Ошибка подключения: " . $e->getMessage());
+}
+
+session_start();
+?>
