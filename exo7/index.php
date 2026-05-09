5fdffc05c5a488e2d5b02d7671dbbaf52c39964b
yakini10 <temeaissataditeyakine@gmail.com>
2026-05-10 01:35:10 +0300

    Update index.php

--- exo7/index.php
+++ exo7/index.php
@@ -1,1 +1,69 @@
+<?php
+            foreach ($languages as $lang) {
+                $stmt = $db->prepare(
+                    "INSERT INTO application_languages(application_id, language_id)
+                    VALUES (?, ?)"
+                );
 
+                $stmt->execute([
+                    $_SESSION['application_id'],
+                    (int)$lang
+                ]);
+            }
+
+        } else {
+
+            $stmt = $db->prepare(
+                "INSERT INTO application
+                (fio, phone, email, birth_date, gender, biography, contract_accepted)
+                VALUES (?, ?, ?, ?, ?, ?, ?)"
+            );
+
+            $stmt->execute([
+                $fio,
+                $phone,
+                $email,
+                $birth_date,
+                $gender,
+                $biography,
+                1
+            ]);
+
+            $app_id = $db->lastInsertId();
+
+            foreach ($languages as $lang) {
+
+                $stmt = $db->prepare(
+                    "INSERT INTO application_languages(application_id, language_id)
+                    VALUES (?, ?)"
+                );
+
+                $stmt->execute([$app_id, (int)$lang]);
+            }
+
+            $login = 'user' . random_int(1000, 9999);
+            $password = bin2hex(random_bytes(4));
+
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+
+            $stmt = $db->prepare(
+                "INSERT INTO users(login, password_hash, application_id)
+                VALUES (?, ?, ?)"
+            );
+
+            $stmt->execute([$login, $hash, $app_id]);
+        }
+
+        $db->commit();
+
+        $messages[] = '<div class="success">Data saved successfully</div>';
+
+    } catch (Exception $e) {
+
+        $db->rollBack();
+        $messages[] = '<div class="error">Database error</div>';
+    }
+
+    include('form.php');
+}
+?>
